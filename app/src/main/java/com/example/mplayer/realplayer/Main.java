85f427c9bb011cfc155ecf6a96040e7a50cac1f9
Vasil Vasilev <vasilvasilev093@gmail.com>
2015-04-26 04:16:40 +0300

    Added method setOnComplete..

--- app/src/main/java/com/example/mplayer/realplayer/Main.java
+++ app/src/main/java/com/example/mplayer/realplayer/Main.java
@@ -1,92 +1,97 @@
 package com.example.mplayer.realplayer;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListView;
 
-import java.util.Random;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 
-public class Main extends ListActivity implements MediaPlayer.OnPreparedListener {
+public class Main extends Activity implements MediaPlayer.OnPreparedListener {
 
     MediaPlayer myPlayer;
     ImageButton play_pause;
     ImageButton next;
     boolean isPlayerStarted = false;
     boolean started = false;
-    List<String> songs = new ArrayList<String>();
+    int current_song = 0;
+    private Map<BPM, List<File>> songsByBPM = new HashMap<BPM, List<File>>();
 
     @Override
     public void onPrepared(MediaPlayer mp) {
         myPlayer.start();
     }
 
-    private enum BPM {
-        L1(80), L2(100), L3(120), L4(130), L5(140), L6(150), L7(160), L8(170), L9(180), L10(190);
-
-        private Integer bpmRate;
-
-        private BPM(Integer bpmRate) {
-            this.bpmRate = bpmRate;
-        }
-    }
-
     ;
-
-    private Map<BPM, List<File>> songsByBPM = new HashMap<BPM, List<File>>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        try {
-            updatePlaylist();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        myPlayer = new MediaPlayer();
 
         play_pause = (ImageButton) findViewById(R.id.btnPlayPause);
         next = (ImageButton) findViewById(R.id.btnNextSong);
 
-        play_pause.setOnClickListener(new View.OnClickListener(){
+        final Main that = this;
+        myPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer player) {
+
+                Random random_position = new Random();
+                Field[] fields = R.raw.class.getFields();
+                Integer rand = random_position.nextInt(fields.length);
+                while(current_song == rand){
+                    rand = random_position.nextInt(fields.length);
+                }
+                current_song = rand;
+                player.stop();
+                player.reset();
+                try {
+                    player.setDataSource(that,
+                            Uri.parse("android.resource://com.example.mplayer.realplayer/" + fields[rand].getInt(Integer.class)));
+                } catch (Exception e) {
+
+                }
+                try {
+                    player.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                player.start();
+            }
+        });
+
+        play_pause.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View s) {
-                /*int current_position = myPlayer.getCurrentPosition();
-                Random random_position = new Random();
-                Field[] fields = R.raw.class.getFields();
-                int position = random_position.nextInt(fields.length);*/
-                if(!started){
+                if (!started) {
                     Random random_position = new Random();
                     Field[] fields = R.raw.class.getFields();
                     int position = random_position.nextInt(fields.length);
+                    current_song = position;
                     changeSong(position);
                     started = true;
-                }
-                else{
-                    if (myPlayer.isPlaying()){
+                } else {
+                    if (myPlayer.isPlaying()) {
                         myPlayer.pause();
-                    }
-                    else{
+                    } else {
                         myPlayer.start();
                     }
                 }
@@ -96,30 +101,31 @@ public class Main extends ListActivity implements MediaPlayer.OnPreparedListener
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int current_position = myPlayer.getCurrentPosition();
+                if(!started){
+                    return;
+                }
                 Random random_position = new Random();
                 Field[] fields = R.raw.class.getFields();
                 int position = random_position.nextInt(fields.length);
-                while(current_position == position){
+                while (current_song == position) {
                     position = random_position.nextInt(fields.length);
                 }
-
+                current_song = position;
                 changeSong(position);
             }
         });
     }
 
     private void changeSong(int position) {
-        if(isPlayerStarted){
+        if (isPlayerStarted) {
             myPlayer.pause();
         }
         try {
             isPlayerStarted = true;
             Field[] fields = R.raw.class.getFields();
-            myPlayer = new MediaPlayer();
+            //myPlayer = new MediaPlayer();
             myPlayer.reset();
-            /*String a1 = fields[0].getName();
-            String a2 = fields[0].toString();*/
+
             Uri uri = Uri.parse("android.resource://com.example.mplayer.realplayer/" + fields[position].getInt(Integer.class));
             myPlayer.setDataSource(this, uri);
             myPlayer.prepare();
@@ -129,21 +135,6 @@ public class Main extends ListActivity implements MediaPlayer.OnPreparedListener
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-
-
-
-    private void updatePlaylist() throws IllegalAccessException {
-
-        Field[] fields = R.raw.class.getFields();
-
-        for (Field field : fields) {
-            songs.add(field.getName());
-        }
-
-        ArrayAdapter<String> songList = new ArrayAdapter<String>(this, R.layout.song_item, songs);
-        setListAdapter(songList);
     }
 
     @Override
@@ -182,6 +173,16 @@ public class Main extends ListActivity implements MediaPlayer.OnPreparedListener
         if (myPlayer != null) {
             myPlayer.release();
             myPlayer = null;
+        }
+    }
+
+    private enum BPM {
+        L1(80), L2(100), L3(120), L4(130), L5(140), L6(150), L7(160), L8(170), L9(180), L10(190);
+
+        private Integer bpmRate;
+
+        private BPM(Integer bpmRate) {
+            this.bpmRate = bpmRate;
         }
     }
 }
